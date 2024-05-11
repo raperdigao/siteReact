286dd974eda08b90e3b760edd2e1b960508659b0
Raphael <ra.perdigao@gmail.com>
2024-05-11 15:33:18 -0300

    criar cards e estilizar

---
+++ src/Components/Cards/Cards.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Cards.css'
+
+const Cards = () => {
+    return (
+        <div className='allCards'>
+            <div className='card'>
+                <h1 className='card_text'>
+
+                </h1>
+                <p className='card_info'>
+
+                </p>
+                <button className='card_button'>
+
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Cards;
